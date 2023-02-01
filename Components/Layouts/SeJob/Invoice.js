cdb30f6b082048dd5a21385fa0d333cbc2f63ff0
DESKTOP-GN1I4I1\HPP <syedabdullahteamhail@gmail.com>
2023-02-01 16:19:39 +0500

    update

--- Components/Layouts/SeJob/Invoice.js
+++ Components/Layouts/SeJob/Invoice.js
@@ -1,25 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import InvoiceCharges from '../../Shared/InvoiceCharges';
+import { Spinner } from 'react-bootstrap';
 
 const Invoice = ({state, dispatch}) => {
+
+    const [load, setLoad] = useState(false);
 
     useEffect(() => {
         getData();
     }, [state.selectedInvoice])
 
     const getData = async() => {
+        setLoad(true)
         await axios.get(process.env.NEXT_PUBLIC_CLIMAX_GET_INVOICE_BY_NO,{
             headers:{"invoice_No": `${state.selectedInvoice}`}
         }).then((x)=>{
             //console.log(x.data)
+            setLoad(false)
             dispatch({type:'toggle', fieldName:'invoiceData', payload:x.data.result})
         })
     }
 
   return (
     <div style={{minHeight:680}}>
-        <InvoiceCharges data={state.invoiceData} />
+        {!load && <InvoiceCharges data={state.invoiceData} />}
+        {load && <div style={{textAlign:"center", paddingTop:'30%'}}><Spinner /></div>}
     </div>
   )
 }
