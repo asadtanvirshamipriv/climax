5fcb20b61d92f7ceb140a0c7c6a38b5c9b53c88f
DESKTOP-GN1I4I1\HPP <syedabdullahteamhail@gmail.com>
2023-05-09 16:23:07 +0500

    update

--- pages/seJob/[id].js
+++ pages/seJob/[id].js
@@ -1,5 +1,6 @@
 import React from 'react';
 //import { useRouter } from 'next/router';
+import axios from "axios";
 import SeJobCopy from '../../Components/Layouts/SE/SeJobCopy';
 
 const seJob = ({jobData, id, fieldsData}) => {
@@ -12,13 +13,11 @@ export default seJob
 export async function getStaticProps(context) {
     const { params } = context;
     let jobData = {};
-
     const fieldsData = await fetch(process.env.NEXT_PUBLIC_CLIMAX_GET_SEAJOB_VALUES).then((x)=>x.json());
     
     if(params.id!="new"){
       jobData = await fetch(process.env.NEXT_PUBLIC_CLIMAX_GET_SE_JOB_BY_ID,{
-        headers:{ "id": `${params.id}` },
-        cache: 'no-store' 
+        headers:{ "id": `${params.id}` }
       }).then((x)=>x.json());//.then((x)=>x.data.result);
     
       if (!jobData.result.id) {
@@ -27,13 +26,22 @@ export async function getStaticProps(context) {
         }
       }
     }
+    // return {
+    //   props: { jobData:jobData.result, id:params.id, fieldsData:fieldsData,  }
+    // }
     return {
-      props: { jobData:jobData.result, id:params.id, fieldsData:fieldsData,  }
-    }
+      props: { 
+        jobData:jobData.result, id:params.id, fieldsData:fieldsData
+      },
+      // Next.js will attempt to re-generate the page:
+      // - When a request comes in
+      // - At most once every 10 seconds
+      revalidate: 10, // In seconds
+    };
   }
   
 export async function getStaticPaths() {
-  const response = await fetch(process.env.NEXT_PUBLIC_CLIMAX_GET_SE_JOBS_IDS, { cache: 'no-store' });
+  const response = await fetch(process.env.NEXT_PUBLIC_CLIMAX_GET_SE_JOBS_IDS);
   //console.log(response, "Over Here")
   const data = await response.json();
   const paths = data.result.map(x => {
@@ -42,8 +50,5 @@ export async function getStaticPaths() {
     }
   })
 
-  return {
-    paths,
-    fallback: true
-  }
+  return { paths, fallback: 'blocking' };
 }
